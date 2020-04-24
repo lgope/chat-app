3e7750781a5573c481d88b497a7f00cdfde24303
lgope <ilakshmangope@gmail.com>
2020-04-24 23:27:29 +0600

    added chalk and updated packages 🚀

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,17 +1,17 @@
 const generateMessage = (username, text) => {
-    return {
-        username,
-        text,
-        createdAt: new Date().getTime()
-    };
+  return {
+    username,
+    text,
+    createdAt: new Date().getTime(),
+  };
 };
 
 const generateLocationMessage = (username, url) => {
-    return {
-        username,
-        url,
-        createdAt: new Date().getTime()
-    };
+  return {
+    username,
+    url,
+    createdAt: new Date().getTime(),
+  };
 };
 
 module.exports = { generateMessage, generateLocationMessage };
--- src/utils/users.js
+++ src/utils/users.js
@@ -7,51 +7,51 @@ const users = [];
 // getUsersInRoom -> will be allowing us to get a complete list of all of the users in a specific room and also that's going to allow us to eventually render that users list to the sidebar.
 
 const addUser = ({ id, username, room }) => {
-    // Clean the data
-    username = username.trim().toLowerCase(); // used trim for remove spaces before and after and toLowerCase used for make sure that those user names are not case sensitive.
+  // Clean the data
+  username = username.trim().toLowerCase(); // used trim for remove spaces before and after and toLowerCase used for make sure that those user names are not case sensitive.
 
-    room = room.trim().toLowerCase();
+  room = room.trim().toLowerCase();
 
-    // Validate the data
-    if (!username || !room) {
-        return {
-            error: "Username and room are required!"
-        };
-    }
+  // Validate the data
+  if (!username || !room) {
+    return {
+      error: 'Username and room are required!',
+    };
+  }
 
-    // Check for existing user
-    const existingUser = users.find((user) => {
-        return user.room === room && user.username === username;
-    });
+  // Check for existing user
+  const existingUser = users.find((user) => {
+    return user.room === room && user.username === username;
+  });
 
-    // Validate username
-    if (existingUser) {
-        return {
-            error: "Username is in use!"
-        };
-    }
+  // Validate username
+  if (existingUser) {
+    return {
+      error: 'Username is in use!',
+    };
+  }
 
-    // Store user
-    const user = { id, username, room };
-    users.push(user);
-    return { user };
+  // Store user
+  const user = { id, username, room };
+  users.push(user);
+  return { user };
 };
 
 const removeUser = (id) => {
-    const index = users.findIndex(user => user.id === id);
+  const index = users.findIndex((user) => user.id === id);
 
-    if (index !== -1) {
-        return users.splice(index, 1)[0] // splice allows us to remove item's from an array by their index.
-    }
+  if (index !== -1) {
+    return users.splice(index, 1)[0]; // splice allows us to remove item's from an array by their index.
+  }
 };
 
 const getUser = (id) => {
-    return users.find((user) => user.id === id);
+  return users.find((user) => user.id === id);
 };
 
 const getUsersInRoom = (room) => {
-    room = room.trim().toLowerCase();
-    return users.filter((user) => user.room === room);
+  room = room.trim().toLowerCase();
+  return users.filter((user) => user.room === room);
 };
 
 module.exports = { addUser, removeUser, getUser, getUsersInRoom };
